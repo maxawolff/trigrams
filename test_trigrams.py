35c24acb056fd4a84570312a5b7e39a484a3c3b0
kernamx <powen.chang1@gmail.com>
2017-10-19 14:51:36 -0700

    trying to split the list on words.  still need fix the function

--- test_trigrams.py
+++ test_trigrams.py
@@ -1,2 +1,25 @@
-
+"""."""
 import pytest
+
+
+def test_open_file():
+    """."""
+    from trigrams import processes_input
+    # assert processes_input('sherlock_sample.txt') == True
+    if processes_input('sherlock_sample.txt'):
+        assert True
+    else:
+        assert False
+
+
+words = [(2, 'was'), (6, 'of'), (0, 'One'), (9, 'returning')]
+
+
+@pytest.mark.parametrize('n, result', words)
+def test_sting_list(n, result):
+    """."""
+    from trigrams import input_string_to_list, processes_input
+    test_string = processes_input('sherlock_sample.txt')
+    list = input_string_to_list(test_string)
+    # assert list[n] == result
+    assert list == 'One'
--- trigrams.py
+++ trigrams.py
@@ -2,10 +2,19 @@
 
 
 def processes_input(source_file):
-	"""."""
-	import io
-	f = io.open(source_file, encoding='utf-8')
-	source_txt = f.read()
-	f.close()
-print (source_txt)
-return source_txt
+    """."""
+    import io
+    f = io.open(source_file, encoding='utf-8')
+    source_txt = f.read()
+    f.close()
+    print(source_txt)
+    return source_txt
+
+
+def input_string_to_list(string):
+    """."""
+    list_of_words = string.split(' ')
+    for i in list_of_words:
+        word = i.split('\n')
+        return word
+    return list_of_words
